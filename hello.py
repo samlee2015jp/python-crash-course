17894c62279043147460f10544398346535e24eb
Sam Li <samlee2015jp@gmail.com>
2026-01-21 21:02:26 +0900

    add

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
-message = "Hello Python world!"
-print(message)
+firstname = 'Sam'
+lastname = 'Li'
+fullname = f"{firstname} {lastname}"
+fullname2 = "{} {}".format(firstname, lastname)
+print(f"Hello, {fullname2.title()}")
+universe_age = 14_000_000_000
+print(universe_age)
 
+x, y, z = 0, 1, 2
+print("x is: {}, y is: {}, z is: {}".format(x, y, z))
+
+numbers = [1,2,3,4,5,6]
+print(numbers[5-6])
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
