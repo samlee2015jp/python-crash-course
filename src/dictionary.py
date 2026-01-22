4596d46fecb40639c8a018e93503839512bcf242
Sam Li <samlee2015jp@gmail.com>
2026-01-22 22:13:40 +0900

    add

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,7 +1,16 @@
-alien_0 = {'color': 'green', 'points': 5}
-print(alien_0)
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-print(alien_0)
-for alien in alien_0:
-    print(alien)
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'fast'}
+print(f"Original position: {alien_0['x_position']}")
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+x_increment = 0
+increment = 0
+if alien_0['speed'] == 'slow':
+    increment = 1
+elif alien_0['speed'] == 'medium':
+    increment = 2
+else:
+# This must be a fast alien.
+    x_increment = 3
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
