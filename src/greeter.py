9c798d1b2166e8ea3e18137b3a0a4c9b81091cd8
Sam Li <samlee2015jp@gmail.com>
2026-01-24 18:59:36 +0900

    add

---
+++ src/greeter.py
@@ -0,0 +1,10 @@
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+greet_user("sam")
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
---
+++ src/vector.py
@@ -0,0 +1,4 @@
+v = [1, 2, 3]
+r = v * 2
+print(v)
+print(r)
